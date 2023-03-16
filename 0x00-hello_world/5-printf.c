a162afef268e320667abf2ee10d2b57b4ce931a2
Vasco <kennedykoech048@gmail.com>
2023-03-16 15:27:48 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - main function
+ * description - start of the program
+ * Return: return 0 on success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
