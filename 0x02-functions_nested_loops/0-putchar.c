fd0e997b6b63cdc1074ee1f21cbbb7be19739671
Vasco <kennedykoech048@gmail.com>
2023-03-21 12:00:18 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 /**
-* main - prints _putcchar with a new line
+* main - prints _putchar with a new line
 * Return: 0 on success
 */
 
 int main(void)
 {
-    printf("_putchar\n");
+    printf("_");
+    printf("p");
+    printf("u");
+    printf("t");
+    printf("c");
+    printf("h");
+    printf("a");
+    printf("r");
+    printf("\n");
     return 0;
 }
