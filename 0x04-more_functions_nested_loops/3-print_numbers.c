2bca39326ca7fa5dc61dc18f5bceb9ab0a065b15
Vasco <kennedykoech048@gmail.com>
2023-03-26 03:45:32 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_numbers - prints 0 to 9
+* @c: character to print
+* Return: returns 0
+*/
+
+int print_numbers(void)
+{
+	while (c >= '0' && c <= '9')
+		_putchar(c);
+		_putchar('\n');
+}
