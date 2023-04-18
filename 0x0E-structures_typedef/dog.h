bf693e3b97b9521cfdb4da03d663f6b2e8f2df16
Vasco <kennedykoech048@gmail.com>
2023-04-18 12:47:55 -0700

    Fix all

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,3 @@
-
 #ifndef DOG_H
 #define DOG_H
 #include <stdlib.h>
@@ -21,4 +20,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
-
+#endif
