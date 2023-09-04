bafaef115d074703ec940bd3db451e7c9a36c25d
Vascokenn <kennedykoech048@gmail.com>
2023-09-04 12:07:02 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,20 +12,20 @@ void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Unable to read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Unable to write to %s\n", argv[2]);
 		exit(99);
 	}
 }
 
 /**
- * main - Copy content from one file to another.
- * @argc: Number of command-line arguments.
- * @argv: Command-line arguments vector.
+ * main - A program to copy files.
+ * @argc: Number of arguments.
+ * @argv: Arguments vector.
  * Return: Always returns 0.
  */
 int main(int argc, char *argv[])
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
@@ -58,14 +58,14 @@ int main(int argc, char *argv[])
 	err_close = close(file_from);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Unable to close fd %d\n", file_from);
 		exit(100);
 	}
 
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Unable to close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
---
+++ README.md
@@ -0,0 +1,3 @@
+File I/O in C Programming
+Overview
+This repository contains code examples and explanations for working with File Input/Output (I/O) in the C programming language. File I/O is a fundamental concept in programming, allowing you to read data from and write data to files. This README provides an introduction to the topic and an overview of the contents of this repository.
